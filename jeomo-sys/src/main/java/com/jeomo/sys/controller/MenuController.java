9ed6a7324a278ec83259252148e3a420153b8f6e
qibotian <qibotian@jiuqi.com.cn>
2019-05-29 23:44:41 +0800

    菜单Controller

--- jeomo-sys/src/main/java/com/jeomo/sys/controller/MenuController.java
+++ jeomo-sys/src/main/java/com/jeomo/sys/controller/MenuController.java
@@ -1,8 +1,11 @@
 package com.jeomo.sys.controller;
 
 import com.jeomo.common.controller.BaseController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jeomo.common.vo.DataTable;
+import com.jeomo.sys.entity.Menu;
+import com.jeomo.sys.service.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,5 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sys/menu")
 public class MenuController extends BaseController {
+
+    @Autowired
+    protected IMenuService menuService;
+
+    @PostMapping("/{id}")
+    public Menu get(@PathVariable Long id) {
+        return menuService.getById(id);
+    }
+
+    @PostMapping("/detail/{id}")
+    public Menu detail(@PathVariable Long id) {
+        return menuService.getById(id);
+    }
+
+    @PostMapping("/list")
+    public DataTable<Menu> list(@RequestBody DataTable dt) {
+        return menuService.pageSearch(dt);
+    }
+
+    @PostMapping("/del/{id}")
+    public boolean del(@PathVariable Long id) {
+        return menuService.removeById(id);
+    }
+
+    @PostMapping("/create")
+    public boolean create(@RequestBody Menu menu) {
+        return menuService.save(menu);
+    }
+
+    @PostMapping("/update")
+    public boolean update(@RequestBody Menu menu) {
+        return menuService.updateById(menu);
+    }
 
 }
