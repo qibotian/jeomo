df12474aebf4c2fc7853a077d61548aacf99bc90
qibotian <hashcoder@163.com>
2020-05-27 23:46:07 +0800

    使用redisson实现分布式锁

--- jeomo-flashSale/src/main/java/com/jeomo/flashSale/application/service/impl/OrderServiceImpl.java
+++ jeomo-flashSale/src/main/java/com/jeomo/flashSale/application/service/impl/OrderServiceImpl.java
@@ -8,7 +8,6 @@ import com.jeomo.flashSale.domain.repository.FlashSaleActivityRepository;
 import com.jeomo.flashSale.domain.repository.OrderRepository;
 import com.jeomo.flashSale.interfaces.dto.FlashSaleReqDto;
 import com.jeomo.flashSale.redis.GoodsKey;
-import org.redisson.RedissonRedLock;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,28 +64,40 @@ public class OrderServiceImpl implements OrderService {
         //使用redisson实现分布式事务锁， 避免了锁超时问题，
         //  但是1： 如果主从结构，在master上设置锁成功了，但是尚未同步到slave节点时，master节点挂了，也会出现超卖问题 解决方案：RedLock
         //  但是2：  单线程串行实现会有性能问题  解决方案： 使用分段锁，使不同段位落在不同的redis节点上
-//        RLock lock = redissonClient.getLock(goodsKey.getKey());
-//        lock.lock();
-//        try {
-//            return  miaosha(dto);
-//        } finally {
-//            lock.unlock();
-//        }
+        RLock lock = redissonClient.getLock(goodsKey.getKey());
+        lock.lock();
+        try {
+            return  miaosha(dto);
+        } finally {
+            lock.unlock();
+        }
 
         //使用RedLock实现分布式锁
         // Redlock 实现原理是，只有集群中半数以上的节点加锁成功，才算加锁成功。 如果加锁失败，则需要回滚
         // 缺点： 假如有5个节点ABCDE， 线程1 成功锁定了ABC 三个节点， 但是此时C节点挂了，导致C节点的数据没有来得及刷盘， 如果C节点重启，线程2又锁定了CDE三个节点，就会导致两个线程同时访问同一个共享资源。
         // 解决方法： 延时重启，即节点挂了以后，不要立即重启，而是稍过一段时间重启。
-        RLock lock = redissonClient.getLock(goodsKey.getKey());
-        RLock lock2 = redissonClient.getLock(goodsKey.getKey());
-        RLock lock3 = redissonClient.getLock(goodsKey.getKey());
-        RedissonRedLock redLock = new RedissonRedLock(lock, lock2, lock3);
-        redLock.lock();
-        try {
-            return  miaosha(dto);
-        } finally {
-            redLock.unlock();
-        }
+
+//        Config config = new Config();
+//        config.useSentinelServers()
+//                .addSentinelAddress("127.0.0.1:6379","127.0.0.1:6389", "127.0.0.1:6399")
+//                .setMasterName("master")
+//                .setPassword("password")
+//                .setDatabase(0);
+//        RedissonClient client = Redisson.create(config);
+//        RLock lock = client.getLock(goodsKey.getKey());
+//        RLock lock2 = client.getLock(goodsKey.getKey());
+//        RLock lock3 = client.getLock(goodsKey.getKey());
+//        RedissonRedLock redLock = new RedissonRedLock(lock, lock2, lock3);
+//        try {
+//            boolean hasLock = redLock.tryLock(500, TimeUnit.MICROSECONDS);
+//            if(hasLock) {
+//                return  miaosha(dto);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } finally {
+//            redLock.unlock();
+//        }
     }
 
 
